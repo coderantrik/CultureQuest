f9e01d702e997b1470857e3a99814d24027ece80
Antriksh Bhadauriya <120263006+coderantrik@users.noreply.github.com>
2025-05-25 19:57:43 +0530

    Update snowflake_connection.py

--- utils/snowflake_connection.py
+++ utils/snowflake_connection.py
@@ -1,5 +1,7 @@
 # utils/snowflake_connection.py
 
+# utils/snowflake_connection.py
+
 import streamlit as st
 import snowflake.connector
 
@@ -7,11 +9,11 @@ import snowflake.connector
 def create_connection():
     sf_secrets = st.secrets["snowflake"]
     conn = snowflake.connector.connect(
-        user=sf_secrets["ANTRIK09"],
-        password=sf_secrets["Akshansh@7523869509"],
-        account=sf_secrets["HJKVCXU-UP61395"],
-        warehouse=sf_secrets["COMPUTE_WH"],
-        database=sf_secrets["CULTUREQUEST_DB"],
-        schema=sf_secrets["PUBLIC"]
+        user=sf_secrets["user"],
+        password=sf_secrets["password"],
+        account=sf_secrets["account"],
+        warehouse=sf_secrets["warehouse"],
+        database=sf_secrets["database"],
+        schema=sf_secrets["schema"]
     )
     return conn
